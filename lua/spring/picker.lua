f4613af691595716ab7f7f6b85782e804ec46352
zerochae <zerochae@kakao.com>
2024-04-20 20:11:22 +0900

    ✨ Feat: picker refactor

--- lua/spring/picker.lua
+++ lua/spring/picker.lua
@@ -1,20 +1,29 @@
 local pickers = require "telescope.pickers"
 local spring_finder = require "spring.finder"
 local spring_previewer = require "spring.previewer"
+local U = require "spring.util"
 local E = require "spring.enum"
 
+local create_find_table = function(annotation)
+  U.create_request_mapping_table()
+  U.create_spring_table(annotation)
+end
+
 local spring_picker = function(opts, method)
-  local finder_by_method = spring_finder(method)
-  local previewer_by_method = spring_previewer(method)
+  local annotation = U.get_annotation(method)
+
+  create_find_table(annotation)
+  local finder = spring_finder(annotation)
+  local previewer = spring_previewer()
 
   return pickers.new(opts, {
-    finder = finder_by_method,
-    -- previewer = previewer_by_method,
+    finder = finder,
+    previewer = previewer,
   })
 end
 
 return function(opts, method)
   opts = opts or {}
-  method = method or E.methods.ALL
+  method = method or E.methods.GET
   spring_picker(opts, method):find()
 end
