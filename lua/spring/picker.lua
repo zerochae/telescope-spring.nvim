1c6ed72429fe3e7ae63dac1bfe5505d74afd6723
zerochae <zerochae@kakao.com>
2024-04-21 13:25:55 +0900

    ✨ Feat: sorter

--- lua/spring/picker.lua
+++ lua/spring/picker.lua
@@ -1,6 +1,7 @@
 local pickers = require "telescope.pickers"
 local spring_finder = require "spring.finder"
 local spring_previewer = require "spring.previewer"
+local conf = require("telescope.config").values
 local U = require "spring.util"
 
 return function(opts, method)
@@ -8,11 +9,13 @@ return function(opts, method)
   local annotation = U.get_annotation(method)
   local finder = spring_finder(annotation)
   local previewer = spring_previewer(annotation)
+  local sorter = conf.generic_sorter()
 
   return pickers
     .new(opts, {
       finder = finder,
       previewer = previewer,
+      sorter = sorter,
     })
     :find()
 end
